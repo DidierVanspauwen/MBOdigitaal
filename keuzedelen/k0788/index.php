3aae80fee10bdb3a5cb75c724bab2cf02f56a6b1
DidierVanspauwen <113337439+DidierVanspauwen@users.noreply.github.com>
2024-09-19 09:16:38 +0200

    .

---
+++ keuzedelen/k0788/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <?php require $_SERVER["DOCUMENT_ROOT"] . '/views/templates/head.php'; ?>
+</head>
+
+<body>
+    <?php require $_SERVER["DOCUMENT_ROOT"] . '/views/templates/menu.php' ?>
+
+    <div class="mt-6 mb-16 w-11/12 p-6 space-y-8 sm:p-8 bg-white mx-auto flex justify-center">
+        <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+            <h1 class="text-2xl font-bold text-gray-800 mb-4">Keuzedelen</h1>
+            <ul class="list-disc pl-5 space-y-2 text-gray-700">
+                <li>Basis Programmeren</li>
+            </ul>
+        </div>
+    </div>
+    
+        <?php require $_SERVER["DOCUMENT_ROOT"] . '/views/templates/footer.php' ?>
+
+    </div>
+
+</body>
+
+</html>
--- views/keuzedelen/keuzedelen.php
+++ views/keuzedelen/keuzedelen.php
@@ -9,17 +9,18 @@
     <?php require $_SERVER["DOCUMENT_ROOT"] . '/views/templates/menu.php' ?>
 
     <div class="mt-6 mb-16 w-11/12 p-6 space-y-8 sm:p-8 bg-white mx-auto">
-    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h1 class="text-2xl font-bold text-gray-800 mb-4">Keuzedelen</h1>
-        <ul class="list-disc pl-5 space-y-2 text-gray-700">
-            <li><a href='/keuzedelen/k0788/k0788.php'>Basis Programmeren</a></li>
-            <li><a href='/keuzedelen/k0023/k0023.php'>Digitale vaardigheden gevorderd</a></li>
-            <li><a href='/keuzedelen/k0495/k0495.php'>Fotografie basis</a></li>
-            <li><a href='/keuzedelen/k0226/k0226.php'>Inspelen op innovaties</a></li>
-            <li><a href='/keuzedelen/k0072/k0072.php'>Ondernemend gedrag</a></li>
-            <li><a href='/keuzedelen/k0080/k0080.php'>Orientatie op ondernemendschap</a></li>
-            <li><a href='/keuzedelen/k0505/k0505.php'>Verdieping software</a></li>
-        </ul>
+        <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md mx-auto">
+            <h1 class="text-2xl font-bold text-gray-800 mb-4">Keuzedelen</h1>
+            <ul class="list-disc pl-5 space-y-2 text-gray-700">
+                <li><a href='/keuzedelen/k0788/index.php'>Basis Programmeren</a></li>
+                <li><a href='/keuzedelen/k0023/index.php'>Digitale vaardigheden gevorderd</a></li>
+                <li><a href='/keuzedelen/k0495/index.php'>Fotografie basis</a></li>
+                <li><a href='/keuzedelen/k0226/index.php'>Inspelen op innovaties</a></li>
+                <li><a href='/keuzedelen/k0072/index.php'>Ondernemend gedrag</a></li>
+                <li><a href='/keuzedelen/k0080/index.php'>Orientatie op ondernemendschap</a></li>
+                <li><a href='/keuzedelen/k0505/index.php'>Verdieping software</a></li>
+            </ul>
+        </div>
     </div>
 
         <?php require $_SERVER["DOCUMENT_ROOT"] . '/views/templates/footer.php' ?>
